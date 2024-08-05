d6822ece5499ba27c32fec43d66d0be0258f0c2e
TahaErtugrul <tahaertugruluzum@gmail.com>
2024-08-04 17:58:59 -0700

    Logout Feature is completed. some changes made to the login as well, corrected the spelling

--- steps/logoutSteps.js
+++ steps/logoutSteps.js
@@ -5,3 +5,21 @@ import { PageManager } from "../globalPagesSetup.js";
 
 
 // WRITE YOUR STEP DEFINITIONS HERE...
+
+Given('user is already logged in as {string}', async function (string) {
+ await PageManager.loginPage(string);
+});
+
+When('user clicks the user profile on top right corner of the page', async function () {
+await PageManager.dashboardPage.userprofileImage.click();
+});
+
+When('clicks the Logout button', async function () {
+    await PageManager.dashboardPage.logoutButton.click();
+
+});
+
+Then('user should be logged out from the app', async function () {
+    await expect(PageManager.page).toHaveTitle("Login - Library");
+  
+});
